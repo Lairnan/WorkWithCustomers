ff11a4b7ac8fc2c8b0f04ffa4a56c0840ddff4fd
Alexey <alexey.saulin03@mail.ru>
2023-02-03 00:11:56 +0300

    Add MainWindow Add TopPanelControl Add AuthWindow Change logo by expand image and text Add main and window styles

---
+++ INCOMSYSTEM/Pages/AuthPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace INCOMSYSTEM.Pages
+{
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ INCOMSYSTEM/Windows/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Controls;
+using System.Windows.Navigation;
+using INCOMSYSTEM.Pages;
+
+namespace INCOMSYSTEM.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AuthFrame = AFrame;
+            AuthFrame.Navigated += AuthFrameOnNavigated;
+            AuthFrame.Navigate(new AuthPage());
+        }
+
+        private void AuthFrameOnNavigated(object sender, NavigationEventArgs e)
+        {
+            var page = ((Frame)sender).Content as Page;
+            this.Title = page?.Title;
+        }
+
+        public static Frame AuthFrame { get; private set; }
+    }
+}
