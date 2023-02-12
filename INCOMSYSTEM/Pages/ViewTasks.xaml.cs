158db744318fc6bdeb8def994de818fad0c8c1cf
Alexey <alexey.saulin03@mail.ru>
2023-02-12 12:26:58 +0300

    Add new main window Add viewtasks page

---
+++ INCOMSYSTEM/Pages/ViewTasks.xaml.cs
@@ -0,0 +1,22 @@
+﻿using INCOMSYSTEM.Windows;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace INCOMSYSTEM.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ViewTasks.xaml
+    /// </summary>
+    public partial class ViewTasks : Page
+    {
+        public ViewTasks()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.MainFrame.Navigate(new ViewTasks());
+        }
+    }
+}
--- INCOMSYSTEM/Windows/MainWindow.xaml.cs
+++ INCOMSYSTEM/Windows/MainWindow.xaml.cs
@@ -1,28 +1,64 @@
-﻿using System.Windows.Controls;
+﻿using INCOMSYSTEM.Pages;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Navigation;
-using INCOMSYSTEM.Pages;
+using System.Windows.Media.Animation;
+using System;
+using System.Threading.Tasks;
 
 namespace INCOMSYSTEM.Windows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            AuthFrame = AFrame;
-            AuthFrame.Navigated += AuthFrameOnNavigated;
-            AuthFrame.Navigate(new AuthPage());
+            MainFrame = MFrame;
+            MainFrame.Navigated += MainFrameOnNavigated;
+            MainFrame.Navigate(new ViewTasks());
         }
 
-        private void AuthFrameOnNavigated(object sender, NavigationEventArgs e)
+        private void MainFrameOnNavigated(object sender, NavigationEventArgs e)
         {
+            Nav(sender);
+            BackBtn.IsEnabled = MainFrame.CanGoBack;
+        }
+
+        private async void Nav(object sender)
+        {
+            var anim = new DoubleAnimation();
+            anim.EasingFunction = new SineEase() { EasingMode = EasingMode.EaseInOut };
+
+            anim.To = 0d;
+            anim.Duration = TimeSpan.FromSeconds(0.7);
+            MFrame.BeginAnimation(OpacityProperty, anim);
+
+            await Task.Delay(700);
+
             var page = ((Frame)sender).Content as Page;
             this.Title = page?.Title;
+
+            await Task.Delay(25);
+
+            anim.To = 1.0d;
+            anim.Duration = TimeSpan.FromSeconds(0.7);
+            MFrame.BeginAnimation(OpacityProperty, anim);
         }
 
-        public static Frame AuthFrame { get; private set; }
+        public static Frame MainFrame { get; private set; }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.GoBack();
+        }
+
+        private void QuitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            new AuthWindow().Show();
+            this.Close();
+        }
     }
 }
