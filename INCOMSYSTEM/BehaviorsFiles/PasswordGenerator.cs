f33dcdc5c0ada79c5c1b4b9aa569b913f3aa258d
Alexey <alexey.saulin03@mail.ru>
2023-05-26 00:29:09 +0300

    Fixed throw stack overflow by generate new password

--- INCOMSYSTEM/BehaviorsFiles/PasswordGenerator.cs
+++ INCOMSYSTEM/BehaviorsFiles/PasswordGenerator.cs
@@ -7,17 +7,19 @@ namespace INCOMSYSTEM.BehaviorsFiles
         public static string GetNewPassword()
         {
             const string text = "abcdefghijklmnopqrstuvwxyz" +
-                               "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-                               "0123456789" +
-                               "!@#$%^&*()";
+                                "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                                "0123456789" +
+                                "!@#$%^&*()";
 
             var random = new Random();
             var newPassword = "";
-            while(string.IsNullOrWhiteSpace(newPassword)
-                  || PasswordDifficulty.CheckDifficultyPassword(newPassword) == DifficultyPassword.Easy)
+            while (string.IsNullOrWhiteSpace(newPassword)
+                   || PasswordDifficulty.CheckDifficultyPassword(newPassword) == DifficultyPassword.Easy)
+            {
+                newPassword = "";
                 for (var i = 0; i < random.Next(5, 15); i++)
                     newPassword += text[random.Next(0, text.Length)];
-            
+            }
             return newPassword;
         }
     }
